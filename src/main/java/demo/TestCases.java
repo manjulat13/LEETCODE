8f465af519921157cec31e9dd08a12f61011fb98
manjulat13 <manjulat13@gmail.com>
2024-03-07 15:26:38 +0530

    ME_QA_XLEETCODE1_MODULE_L1

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -1,7 +1,10 @@
 package demo;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -48,8 +51,76 @@ public class TestCases {
     
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
-        driver.get("https://www.google.com");
-        System.out.println("end Test case: testCase02");
+        driver.get("https://leetcode.com/");
+        String url=driver.getCurrentUrl();
+        if(url.contains("leetcode")){
+            System.out.println("The URL of the Leetcode homepage contains leetcode");
+        }
+        else{
+            System.out.println("Url Not conatins Leetcode");
+        }
+        System.out.println("end Test case: testCase01");
+    }
+    public void testCase02() throws InterruptedException{
+        driver.get("https://leetcode.com/");
+        driver.findElement(By.xpath("//p[contains(text(),'View Questions ')]")).click();
+        Thread.sleep(3000);
+        String problemSet=driver.getCurrentUrl();
+        if(problemSet.contains("problemset")){
+            System.out.println("URL contains problem set");
+        }else{
+            System.out.println("URL doesn't contains problem set");
+
+        }
+        List<WebElement> questions=driver.findElements(By.xpath("//a[contains(@href,'/problems/')]"));
+        for(WebElement problem:questions){
+            int count=0;
+            if(count<5){
+                String text=problem.getText();
+                if(text.contains("Two Sum")||text.contains("Add Two Numbers")||text.contains("Longest Substring")||text.contains("Median of Two Sorted Arrays")||text.contains("Longest Palindromic Substring")){
+                System.out.println(text);
+                
+                }
+                count++;
+            }else{
+                break;
+            }
+        }
+        
+    }
+    public void testCase03() throws InterruptedException{
+        driver.get("https://leetcode.com/");
+        driver.findElement(By.xpath("//p[contains(text(),'View Questions ')]")).click();
+        Thread.sleep(3000);
+        WebElement twoSum=driver.findElement(By.xpath("//a[@href='/problems/two-sum']"));
+        twoSum.click();
+        Thread.sleep(3000);
+        String url=driver.getCurrentUrl();
+        if(url.contains("two-sum")){
+            System.out.println("The URL of the problem contains two-sum");
+        }else{
+            System.out.println("The URL of the problem not contains two-sum");
+
+        }
+
+    }
+    public void testCase04() throws InterruptedException{
+        driver.get("https://leetcode.com/");
+        driver.findElement(By.xpath("//p[contains(text(),'View Questions ')]")).click();
+        Thread.sleep(3000);
+        WebElement twoSum=driver.findElement(By.xpath("//a[@href='/problems/two-sum']"));
+        twoSum.click();
+        Thread.sleep(3000);
+        driver.findElement(By.id("submissions_tab")).click();
+        WebElement regorSign=driver.findElement(By.xpath("//a[contains(@class,'dark:hover:bg-dark-green')]"));
+        String text=regorSign.getText();
+        if(text.contains("Register or Sign In")){
+            System.out.println("The message Register or Sign In is displayed when you click on the submissions tab.");
+        }
+        else{
+            System.out.println("The message Register or Sign In is not displayed when you click on the submissions tab.");
+        }
+
     }
 
 
